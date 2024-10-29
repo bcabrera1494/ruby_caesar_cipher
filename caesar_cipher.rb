02053c92708cf102279fd584de2fc03a2a7d7516
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-29 14:09:26 -0700

    Write logic for caesar_cipher method following the commented out pseudocode. Currently converts the original string into an array.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,27 +1,44 @@
 # Make an hash of alphabet a-z
 # Add keys 1-26 for each letter of the alphabet
 
-:alphabet = {1: 'a', 2: 'b', 3: 'c,' 4: 'd', 5: 'e', 6: 'f', 
-7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 
-13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 
-19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 
-25: 'y', 26: 'z'}
+
 
 :alphabet_plain = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'
 'u', 'v', 'w', 'x', 'y', 'z'}
 
-# Write logic for cipher
 def caesar_cipher(string, key)
-  # Takes string and converts it to an array
+  alphabet_keyed = { 1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd', 5 => 'e', 6 => 'f', 
+    7 => 'g', 8 => 'h', 9 => 'i', 10 => 'j', 11 => 'k', 12 => 'l', 
+    13 => 'm', 14 => 'n', 15 => 'o', 16 => 'p', 17 => 'q', 18 => 'r', 
+    19 => 's', 20 => 't', 21 => 'u', 22 => 'v', 23 => 'w', 24 => 'x', 
+    25 => 'y', 26 => 'z' }
+  original_string_array = string.downcase.chars
+  encoded_string_array = []
+  original_string_array.each do |char|
+    original_key = alphabet_keyed.key(char)
+     char
+     p encoded_key = original_key.to_i + key
+      # if encoded_key > 26
+      #   then encoded_key_looped = encoded_key - 26
+      # end
+    p new_letter = :alphabet_keyed[encoded_key]
+    p encoded_string_array.push(new_letter)
+    p encoded_string_array
+    p encoded_string = encoded_string_array.join(',')
+  end
+  p encoded_string
+end
+
+# Write logic for cipher
+  # Takes string and converts it to an array using #chars
   # Declare an empty variable to hold the modified array
-  # Iterate through the array of the word
-    # For each letter, 
-      # Access index in :alphabet_plain using #find method? 
-      # Then access that letter's index in :alphabet_plain[index+1]
-      # Then put the new letter into a new array using #push method
-      # Return the new array 
+  # Iterate through the array of the word using #each
+  # Access key in :alphabet_keyed using #key method
+  # Then access that letter's key in :alphabet_keyed[key+key]
+     # Add condition if orignal letter's key is greater than 26 The new key is (key+key) - 26
+  # Then put the new letter into a new array using #push method
+  # Return the new array
   # Declare a variable to hold the converted array into a string
   # Convert the new array into a string
   # Return the new string
-end
